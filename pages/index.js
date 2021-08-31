cadf37e08508609014d21c8f07a4332bdf41e219
Ryan Driscoll <ryan@reachcreative.com>
2021-08-31 11:28:18 -0600

    updated page components to use custom input and handle creating new todos and items

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
-import TodoGenerator from '@/components/TodoGenerator';
+import { useEffect, useState } from 'react';
+import { createTodo, getAllTodos } from '@/fetch';
 import Todo from '@/components/Todo';
-import { getAllTodos } from '@/fetch';
+import FindOrCreateInput from '@/components/FindOrCreateInput';
+import useDebounce from '@/hooks/useDebounce';
+import filterList from '@/utils/filterList';
+import useRefreshData from '@/hooks/useRefreshData';
 
 const Todos = ({ todos = [] }) => {
+  const [title, setTitle] = useState('');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const searchTerm = useDebounce(title, 300);
+  const refreshData = useRefreshData();
+
+  useEffect(() => {
+    if (searchTerm) {
+      setFilteredTodos((prevTodos) => filterList({ items: prevTodos, term: searchTerm }));
+    } else {
+      setFilteredTodos(todos);
+    }
+  }, [searchTerm, todos]);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createTodo({ title });
+    setTitle('');
+    refreshData();
+  };
+
   return (
     <div>
-      <TodoGenerator />
-      {todos.map((todo) => (
+      <FindOrCreateInput
+        type="Todo"
+        value={title}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      {filteredTodos.map((todo) => (
         <Todo key={todo.id} {...todo} />
       ))}
     </div>
@@ -17,6 +50,11 @@ export default Todos;
 
 export const getServerSideProps = async () => {
   const todos = await getAllTodos();
+  if (!todos) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
       todos,
--- pages/todos/[id].js
+++ pages/todos/[id].js
@@ -1,11 +1,47 @@
-import ItemGenerator from '@/components/ItemGenerator';
-import { getTodo, getItems } from '@/fetch';
+import { useEffect, useState } from 'react';
+import { getTodo, getItems, createItem } from '@/fetch';
+import useDebounce from '@/hooks/useDebounce';
+import useRefreshData from '@/hooks/useRefreshData';
+import filterList from '@/utils/filterList';
+import FindOrCreateInput from '@/components/FindOrCreateInput';
 
 const TodoDetail = ({ todo, id, items }) => {
+  const [title, setTitle] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
+  const searchTerm = useDebounce(title, 300);
+  const refreshData = useRefreshData();
+
+  useEffect(() => {
+    if (searchTerm) {
+      setFilteredItems((prevItems) => filterList({ items: prevItems, term: searchTerm }));
+    } else {
+      setFilteredItems(items);
+    }
+  }, [searchTerm, items]);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createItem({ id, title });
+    setTitle('');
+    refreshData();
+  };
+
   return (
     <div>
       <h1>{todo?.title}</h1>
-      <ItemGenerator id={id} />
+      <FindOrCreateInput
+        type="Item"
+        value={title}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+      {filteredItems.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
     </div>
   );
 };
@@ -15,6 +51,11 @@ export default TodoDetail;
 export const getServerSideProps = async ({ params }) => {
   const { id } = params;
   const [todo, items] = await Promise.all([getTodo({ id }), getItems({ id })]);
+  if (!todo || !items) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
       todo,

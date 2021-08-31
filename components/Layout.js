b80371d19b7c8170c3e559b0e46b8ac55cb443d5
Ryan Driscoll <ryan@reachcreative.com>
2021-08-31 07:24:25 -0600

    removed footer

--- components/Layout.js
+++ components/Layout.js
@@ -11,16 +11,6 @@ const Layout = ({ children }) => {
       </Head>
 
       <main className={styles.main}>{children}</main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          by Ryan
-        </a>
-      </footer>
     </div>
   );
 };

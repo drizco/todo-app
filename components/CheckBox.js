d3876c006c48047b1bda81948a699fc7d9a7c9c7
Ryan Driscoll <ryan@reachcreative.com>
2021-09-01 11:44:55 -0600

    added custom checkbox component

---
+++ components/CheckBox.js
@@ -0,0 +1,23 @@
+import styles from '@/styles/components/checkbox.module.scss';
+import classNames from 'classnames';
+import { CheckIcon, RoundIcon } from './Icons';
+
+const CheckBox = ({ className, checked, onChange, name }) => {
+  return (
+    <div className={classNames(className, styles.input_container)}>
+      <input
+        type="checkbox"
+        className={styles.hidden_checkbox}
+        name={name}
+        id={name}
+        checked={checked}
+        onChange={onChange}
+      />
+      <RoundIcon className={styles.checkbox} />
+      <CheckIcon className={classNames(styles.check, { [styles.is_checked]: checked })} />
+      <label className={classNames(styles.customCheck)} htmlFor={name}></label>
+    </div>
+  );
+};
+
+export default CheckBox;

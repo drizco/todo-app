77134e1791d610b5aa37956204c49ba23c00d9cd
Ryan Driscoll <ryan@reachcreative.com>
2021-09-01 11:45:22 -0600

    added icon components

---
+++ components/Icons.js
@@ -0,0 +1,60 @@
+import classNames from 'classnames';
+import styles from '@/styles/components/icons.module.scss';
+
+export const CheckIcon = ({ color = '#000000', className = '' }) => (
+  <div className={classNames(styles.icon_wrapper, className)}>
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24px"
+      height="24px"
+    >
+      <path d="M 20.292969 5.2929688 L 9 16.585938 L 4.7070312 12.292969 L 3.2929688 13.707031 L 9 19.414062 L 21.707031 6.7070312 L 20.292969 5.2929688 z" />
+    </svg>
+  </div>
+);
+
+export const RoundIcon = ({ color = '#000000', className = '' }) => (
+  <div className={classNames(styles.icon_wrapper, className)}>
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24px"
+      height="24px"
+    >
+      <path d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z" />
+    </svg>
+  </div>
+);
+
+export const TrashIcon = ({ color = '#000000', className = '' }) => (
+  <div className={classNames(styles.icon_wrapper, className)}>
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24px"
+      height="24px"
+    >
+      {' '}
+      <path d="M 10 2 L 9 3 L 4 3 L 4 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 5 7 L 5 20 C 5 21.1 5.9 22 7 22 L 17 22 C 18.1 22 19 21.1 19 20 L 19 7 L 5 7 z M 8 9 L 10 9 L 10 20 L 8 20 L 8 9 z M 14 9 L 16 9 L 16 20 L 14 20 L 14 9 z" />
+    </svg>
+  </div>
+);
+
+export const PlusIcon = ({ color = '#000000', className = '' }) => (
+  <div className={classNames(styles.icon_wrapper, className)}>
+    <svg
+      fill={color}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24px"
+      height="24px"
+    >
+      {' '}
+      <path d="M12,2C6.477,2,2,6.477,2,12s4.477,10,10,10s10-4.477,10-10S17.523,2,12,2z M17,13h-4v4h-2v-4H7v-2h4V7h2v4h4V13z" />
+    </svg>
+  </div>
+);

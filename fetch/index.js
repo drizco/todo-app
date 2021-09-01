df458056a9b911401d7c61923a78bfa7c77009a2
Ryan Driscoll <ryan@reachcreative.com>
2021-09-01 09:07:28 -0600

    added post and delete functions. catching errors in data fetching functions that are called in getServerSideProps and returning null so gssp can return a not found object

--- fetch/index.js
+++ fetch/index.js
@@ -1,48 +1,72 @@
 import { API_URL } from '@/constants';
 
+// GET
 export const getAllTodos = async () => {
-  const response = await fetch(`${API_URL}/todos`);
-  if (response.ok) {
-    const data = await response.json();
-    return data;
+  try {
+    const response = await fetch(`${API_URL}/todos`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    return null;
   }
 };
 
 export const getTodo = async ({ id }) => {
-  const response = await fetch(`${API_URL}/todos/${id}`);
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  }
-};
-
-export const createTodo = async ({ title }) => {
-  const response = await fetch(`${API_URL}/todos?title=${encodeURIComponent(title)}`, {
-    method: 'POST',
-  });
-  if (response.ok) {
-    const data = await response.json();
-    return data;
+  try {
+    if (!id) throw new Error('id is required');
+    const response = await fetch(`${API_URL}/todos/${id}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    return null;
   }
 };
 
 export const getItems = async ({ id }) => {
-  const response = await fetch(`${API_URL}/todos/${id}/items`);
-  if (response.ok) {
-    const data = await response.json();
-    return data;
+  try {
+    if (!id) throw new Error('id is required');
+    const response = await fetch(`${API_URL}/todos/${id}/items`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (error) {
+    return null;
   }
 };
 
+// POST
+export const createTodo = async ({ title }) => {
+  if (!title) throw new Error('title is required');
+  await fetch(`${API_URL}/todos?title=${encodeURIComponent(title)}`, {
+    method: 'POST',
+  });
+};
+
 export const createItem = async ({ id, title }) => {
-  const response = await fetch(
-    `${API_URL}/todos/${id}/items?title=${encodeURIComponent(title)}`,
-    {
-      method: 'POST',
-    }
-  );
-  if (response.ok) {
-    const data = await response.json();
-    return data;
-  }
+  if (!title) throw new Error('title is required');
+  if (!id) throw new Error('id is required');
+  await fetch(`${API_URL}/todos/${id}/items?title=${encodeURIComponent(title)}`, {
+    method: 'POST',
+  });
+};
+
+// DELETE
+export const deleteTodo = async ({ id }) => {
+  if (!id) throw new Error('id is required');
+  await fetch(`${API_URL}/todos/${id}`, {
+    method: 'DELETE',
+  });
+};
+
+export const deleteItem = async ({ todoId, itemId }) => {
+  if (!todoId) throw new Error('todo id is required');
+  if (!itemId) throw new Error('item id is required');
+  await fetch(`${API_URL}/todos/${todoId}/items/${itemId}`, {
+    method: 'DELETE',
+  });
 };

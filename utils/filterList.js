38581f2a558dcd1c8eca8aca9d0376bb22255db2
Ryan Driscoll <ryan@reachcreative.com>
2021-09-01 17:06:52 -0600

    added escape string function

--- utils/filterList.js
+++ utils/filterList.js
@@ -1,4 +1,15 @@
-const filterList = ({ items, term }) =>
-  items.filter(({ title }) => new RegExp(term, 'i').test(title));
+export const escapeString = (string) => {
+  if (typeof string !== 'string') {
+    throw new Error('Expected a string');
+  }
+  return string.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+};
+
+const filterList = ({ items, term }) => {
+  if (!term) {
+    return items;
+  }
+  return items.filter(({ title }) => new RegExp(escapeString(term), 'i').test(title));
+};
 
 export default filterList;

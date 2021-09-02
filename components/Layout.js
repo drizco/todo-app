6ffb55af95bcbb446b2f4eef4d245dc035cfe2c4
Ryan Driscoll <ryan@reachcreative.com>
2021-09-02 10:26:16 -0600

    added header to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '@/styles/components/layout.module.scss';
 import Error from '@/components/Error';
+import Header from '@/components/Header';
 
 const Layout = ({ children }) => {
   return (
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <main className={styles.main}>
         <div className={styles.page_container}>{children}</div>
         <Error />

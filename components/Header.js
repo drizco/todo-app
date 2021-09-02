16029855b796d6263ec2b4336d37d2f897b730fe
Ryan Driscoll <ryan@reachcreative.com>
2021-09-02 10:25:18 -0600

    added header component

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import styles from '@/styles/components/header.module.scss';
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Death Star Corp</h1>
+    </header>
+  );
+};
+
+export default Header;
